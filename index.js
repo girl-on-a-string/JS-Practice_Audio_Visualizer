5494d26ddb44fdbaedaac9f2eb4e74a5c64b670b
girl-on-a-string <frenchfries55@protonmail.com>
2024-08-06 09:34:49 -0500

    created setup for part 2

--- index.js
+++ index.js
@@ -1,33 +1,16 @@
 //variables
 
-const btnOne = document.getElementById("button-1");
+let audio1 = new Audio();
 const audioCtx = new AudioContext();
 
-console.log(audioCtx);
+const container = document.getElementById("container");
+const canvas = document.getElementById("canvas1");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-//button 1
+const ctx = canvas.getContext('2d');
 
-btnOne.addEventListener("click", () => {
-    audioOne.play();
-    audioOne.addEventListener("playing", () => {
-        console.log("playing");
-    });
-});
+let audioSource;
+let analyzer;
 
-//button 2
-
-const btnTwo = document.getElementById("button-2");
-
-btnTwo.addEventListener("click", () => {
-    playSound();
-});
-
-function playSound() {
-    const oscillator = audioCtx.createOscillator(); //creates audio node that generates a tone
-    oscillator.connect(audioCtx.destination); //destination = default audio rendering device (most likely speakers)
-    oscillator.type = "sine"; //sine square triangle sawtooth
-    oscillator.start();
-    setTimeout(() => {
-        oscillator.stop();
-    }, 1000);
-}
+//
